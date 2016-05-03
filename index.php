f1d480698be6453e1161b9bdfd57ac277d904b27
Pete Kang <kangpeter5@gmail.com>
2016-05-03 11:08:45 -0700

    changed meta description to index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  	<title>Pete Kang - Full Stack Web Developer</title>
+	<meta name="description" content="Pete Kang is a full-stack web developer with experiences in videography, automotive engineering, and social media marketing.">
+</head>
+<body>
+
+</body>
+</html>
