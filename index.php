a53f0e2e9d4452591b3845081edee0c672ce6296
Pete Kang <kangpeter5@gmail.com>
2016-05-04 14:47:42 -0700

    updated xmlns

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-  	<title>Pete Kang - Full Stack Web Developer</title>
-	<meta name="description" content="Pete Kang is a full-stack web developer with experiences in videography, automotive engineering, and social media marketing.">
 </head>
 <body>
 
